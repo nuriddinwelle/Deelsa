9df2001c8672c3d7c67f5dd8089acfe983456bac
Nuriddin <nuriddin@example.com>
2026-01-11 22:47:01 +0500

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "50px", fontFamily: "Arial" }}>
+      <h1>Influencer marketing orqali real mijoz olib kelamiz</h1>
+
+      <p>
+        Biz influencer reklamasini promo code va real tashrif bilan o‘lchaymiz.
+      </p>
+
+      <button>Bepul konsultatsiya</button>
+
+      <h2>Muammo</h2>
+      <p>
+        Influencer reklama qiladi, lekin real mijoz kelgan-kelmaganini bilmaysiz.
+      </p>
+
+      <h2>Yechim</h2>
+      <p>
+        Biz promo code orqali real tashrifni hisoblaymiz.
+      </p>
+      <p>
+Bog‘lanish: +998771304303 <br />
+Telegram: @username <br />
+Email: example@gmail.com
+</p>
+
+    </main>
+  );
+}
+
